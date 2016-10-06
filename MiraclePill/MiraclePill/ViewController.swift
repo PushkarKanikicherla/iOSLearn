ce482ac7147c67d8af58d78a371b564daf9270b6
Kanikicherla <s525021@ol10021m.nwmissouri.edu>
2016-10-05 20:52:47 -0500

    UI and code linkage miracle pill

--- MiraclePill/MiraclePill/ViewController.swift
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var statePicketBtn: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "New York", "Iowa"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnpressed(_ sender: AnyObject) {
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return  states[row]
+    }
 
 }
 
