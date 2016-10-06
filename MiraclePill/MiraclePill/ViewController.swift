2c2e80324789dfcd075a678627fdb2689abee04c
Kanikicherla <s525021@ol10015m.nwmissouri.edu>
2016-10-06 17:52:55 -0500

    Rectified UI with constraints

--- MiraclePill/MiraclePill/ViewController.swift
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController,  UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var statePicketBtn: UIButton!
     @IBOutlet weak var statePicker: UIPickerView!
@@ -25,17 +25,23 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func stateBtnpressed(_ sender: AnyObject) {
+   @IBAction func stateBtnpressed(_ sender: AnyObject) {
+    statePicker.isHidden = false
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return states.count
+       return states.count
     }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return  states[row]
+   func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       return  states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePicketBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
     }
 
 }
